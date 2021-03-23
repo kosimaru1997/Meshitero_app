3b5a280779326f15a81ef3aa79cd6357e89b25db
kosimaru1997 <kosimaru1997@gmail.com>
2021-03-23 17:32:46 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'bb85b7432aa5f2f475a36d8958f8cd05bf72f04b6f45063e32826f1d81d4e2556fdeccbfce8cd03cebb2921ed5ce69f364d269711eb3dd13bbd77140c4a6c4c0'
